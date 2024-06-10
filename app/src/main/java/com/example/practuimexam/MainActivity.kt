57d4f8bbf5ba989315c9747ddb9eafe7f4544b01
ST10452042 <ST10452402@vcconnect.edu.za>
2024-06-10 09:02:47 +0200

    MainActivity first commit

---
+++ app/src/main/java/com/example/practuimexam/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.practuimexam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import java.nio.BufferUnderflowException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val exitButton : Button = findViewById(R.id.exitButton)
+        val proceedButton : Button = findViewById(R.id.proceedButton)
+
+        exitButton.setOnClickListener {
+            finish()
+            //will close the application
+        }
+
+        proceedButton.setOnClickListener {
+            //Start the explicit intent
+            val intent = Intent(this,MainActivity2 ::class.java)
+            //start the activity
+            startActivity(intent)
+        }
+    }
+}
